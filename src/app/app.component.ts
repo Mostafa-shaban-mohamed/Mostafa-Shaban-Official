163d473b534ee8ab1bdf13c11113c6aaf6194415
Mostafa-shaban-mohamed <MostafaShaban4522912@gmail.com>
2023-06-16 18:02:46 +0300

    Adding CA-Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,10 @@
-import { AfterViewInit, Component, OnDestroy } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import {filter} from 'rxjs/operators';
-declare var gtag: Function;
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
-  private routerSubscription: Subscription;
-  
-  constructor(private router: Router) {
-  }
-
-  ngOnDestroy(): void {
-    this.routerSubscription.unsubscribe();
-  }
-  ngAfterViewInit(): void {
-    // subscribe to router events and send page views to Google Analytics
-    this.routerSubscription = this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        gtag('config', 'G-VN3JYPESC3', {'page_path': event.urlAfterRedirects});
-      });
-  }
+export class AppComponent {
   title = 'MyPortfolio';
 }
