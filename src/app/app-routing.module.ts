5ef1cf08e3cf76532d23e2345f0e1d808fd29b13
Mostafa-shaban-mohamed <MostafaShaban4522912@gmail.com>
2023-10-09 21:21:31 +0300

    Adding App Store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppStoreComponent } from './app-store/app-store.component';
+import { IntroPageComponent } from './intro-page/intro-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IntroPageComponent },
+  {path: 'app-store', component: AppStoreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
