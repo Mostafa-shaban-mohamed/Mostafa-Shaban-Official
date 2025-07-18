15cce40241cb8ac6533b1dd48c2ace0b78482ae3
Mostafa-shaban-mohamed <Mostafashaban4522912@gmail.com>
2025-07-18 10:41:46 +0300

    update soical links and experience

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -11,13 +11,13 @@ export class SkillsComponent {
   csharp: number = 5;
   js: number = 4;
   java: number = 2;
-  react: number = 3;
+  react: number = 2;
   htmlcss: number = 4;
   sql: number = 4;
-  angular: number = 3;
+  angular: number = 4;
 
   // softwares & IDEs
-  softwares: string[] = ['Azure', 'VS 2022 / Code', 'Netlify', 'MS SQL (2017 - 2022)'];
+  softwares: string[] = ['Azure', 'VS Studio / Code', 'Netlify', 'MS SQL (2017 - 2024)'];
 
   constructor(){}
 }
